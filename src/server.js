72f623333419abd4d5fb34193bcd2e71e7dd5e8e
Aarush Pathak <apathak9@wisc.edu>
2025-05-28 17:56:53 +0400

    add middleware to server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
-import express from 'express'
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
-const PORT = process.env.PORT || 5003
+const app = express();
+const PORT = process.env.PORT || 5003;
 
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+// MIDDLEWARE
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.static(path.join(__dirname, "../public"))); // Middleware to serve static files from the /public directory
+
+// Serving up the html file from the /public directory
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
