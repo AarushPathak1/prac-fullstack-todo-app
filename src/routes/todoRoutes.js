fef4b85cd637549c672f9b72695d5d1af41f54bc
Aarush Pathak <apathak9@wisc.edu>
2025-05-30 15:36:22 +0400

    Implement create todo functionality

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -11,7 +11,19 @@ router.get("/", (req, res) => {
 });
 
 // Create a new todo
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { task } = req.body;
+  const createTodo = db.prepare(
+    `INSERT INTO todos (user_id, task) VALUES (?, ?)`
+  );
+  const result = createTodo.run(req.userId, task);
+
+  res.json({
+    id: result.lastInsertRowid,
+    task,
+    completed: 0,
+  });
+});
 
 // Update a todo
 router.put("/:id", (req, res) => {});
